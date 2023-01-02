0237790daf1f615927c0d032597ef4fb27454356
이진영 <swlsdud1005@naver.com>
2023-01-02 22:35:40 +0900

    디자인 변경

--- main.js
+++ main.js
@@ -1,29 +1,38 @@
-let taskInput = document.querySelector(".taskInput");
+const taskInput = document.querySelector(".taskInput");
 const addBtn = document.querySelector(".addBtn");
+const taskBoard = document.querySelector(".taskBoard");
 let taskList = [];
 const tabs = document.querySelectorAll(".taskTabs div");
 let mode = "all";
 let filterList = [];
+// const underLine = document.querySelector("#underLine");
+const all = document.querySelector("#all");
+const onGoing = document.querySelector("#ongoing");
+const done = document.querySelector("#done");
 
 addBtn.addEventListener("click", addTask);
-
-for (let i = 1; i < tabs.length; i++) {
-  tabs[i].addEventListener("click", function (e) {
-    filter(e);
-  });
-}
+taskInput.addEventListener("keyup", function (e) {
+  if (e.keyCode === 13) {
+    addTask(e);
+  }
+});
 
 function addTask() {
   let task = {
-    id: randomIDGenerate(),
+    id: randomId(),
     taskContent: taskInput.value,
     isComplete: false,
   };
   if (taskInput.value != "") {
     taskList.push(task);
     taskInput.value = "";
+    taskInput.focus();
     render();
   }
+}
+
+function randomId() {
+  return "_" + Math.random().toString(36).substring(2, 9);
 }
 
 function render() {
@@ -67,7 +76,7 @@ function render() {
       </div>`;
     }
   }
-  document.querySelector(".taskBoard").innerHTML = resultHTML;
+  taskBoard.innerHTML = resultHTML;
 }
 
 function toggleComplete(id) {
@@ -77,49 +86,55 @@ function toggleComplete(id) {
       break;
     }
   }
-  render();
+  filter();
 }
 
 function deleteTask(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id == id) {
       taskList.splice(i, 1);
-      break;
     }
   }
-  render();
+  filter();
+}
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (e) {
+    filter(e);
+  });
 }
 
 function filter(e) {
-  mode = e.target.id;
+  if (e) {
+    mode = e.target.id;
+    if (mode == "all") {
+      all.classList.add("taskClick");
+      onGoing.classList.remove("taskClick");
+      done.classList.remove("taskClick");
+    } else if (mode == "ongoing") {
+      all.classList.remove("taskClick");
+      onGoing.classList.add("taskClick");
+      done.classList.remove("taskClick");
+    } else if (mode == "done") {
+      all.classList.remove("taskClick");
+      onGoing.classList.remove("taskClick");
+      done.classList.add("taskClick");
+    }
+  }
+
   filterList = [];
-
-  document.querySelector("#underLine").style.width =
-    e.target.offsetWidth + "px";
-  document.querySelector("#underLine").style.top =
-    e.target.offsetTop + e.target.offsetHeight + "px";
-  document.querySelector("#underLine").style.left = e.target.offsetLeft + "px";
-
-  if (mode == "all") {
-    render();
-  } else if (mode == "ongoing") {
+  if (mode == "ongoing") {
     for (let i = 0; i < taskList.length; i++) {
       if (taskList[i].isComplete == false) {
         filterList.push(taskList[i]);
       }
     }
-    render();
   } else if (mode == "done") {
     for (let i = 0; i < taskList.length; i++) {
-      if (taskList[i].isComplete == true) {
+      if (taskList[i].isComplete) {
         filterList.push(taskList[i]);
       }
     }
-    render();
   }
-  console.log(filterList);
-}
-
-function randomIDGenerate() {
-  return "_" + Math.random().toString(36).substring(2, 9);
+  render();
 }
